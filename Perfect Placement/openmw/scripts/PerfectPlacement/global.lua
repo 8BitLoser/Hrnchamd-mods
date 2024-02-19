1ed50641952cd31b6d232838c8d9fc8fe48e103a
Hrnchamd <iamalannoble@hotmail.com>
2024-02-19 16:35:19 +0000

    Perfect Placement: (OpenMW) Buffer object teleports in the global script, because an item cannot be teleported more than once in the same frame.

--- Perfect Placement/openmw/scripts/PerfectPlacement/global.lua
+++ Perfect Placement/openmw/scripts/PerfectPlacement/global.lua
@@ -1,21 +1,28 @@
 --[[
-    Mod: Perfect Placement OpenMW
-    Author: Hrnchamd
-    Version: 2.2beta
+	Mod: Perfect Placement OpenMW
+	Author: Hrnchamd
+	Version: 2.2beta
 ]]--
 
 local async = require('openmw.async')
 
+local movement = nil
+
 return {
-    eventHandlers = {
-        ["PerfectPlacement:Move"] = function (e)
-			e.active:teleport(e.active.cell, e.newPosition, e.newRotation)
+	eventHandlers = {
+		["PerfectPlacement:Move"] = function(e)
+			movement = { active = e.active, position = e.newPosition, rotation = e.newRotation }
 		end,
-        ["PerfectPlacement:Drop"] = function (e)
-			-- Use timer to avoid sending a second conflicting teleport in the same frame.
-			async:newUnsavableSimulationTimer(0.05, function()
-				e.active:teleport(e.active.cell, e.newPosition, e.newRotation)
-			end)
+		["PerfectPlacement:Drop"] = function(e)
+			movement = { active = e.active, position = e.newPosition, rotation = e.newRotation }
 		end,
-    }
+	},
+	engineHandlers = {
+		onUpdate = function(dt)
+			if movement then
+				movement.active:teleport(movement.active.cell, movement.position, movement.rotation)
+				movement = nil
+			end
+		end
+	}
 }
